d7781bae06cc2705bd0f693ad13d0ed4baa146c0
Hermilo Cortés <hermilocg@hotmail.com>
2024-02-17 21:31:58 -0600

    sube primera documentación

--- datos/cws/empleo/naics/descarga-ISIC-Rev-4-to-NAICS-2017.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://www.census.gov/naics/concordances/ISIC_4_to_2017_NAICS.xlsx
-wget https://www.census.gov/naics/concordances/ISIC_Rev_4_to_2022_NAICS.xlsx
-wget https://www.census.gov/naics/concordances/2012%20NAICS_to_ISIC_4.xlsx
-wget https://www.census.gov/naics/concordances/2017_to_2012_NAICS_Changes_Only.xlsx
-
-mv *.xlsx datos/
--- datos/empleo/USA/datos/cbp/descarga_cbp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in {11..21}; do 
-    anio=$((2000 + i))
-    URL="https://www2.census.gov/programs-surveys/cbp/datasets/$anio/cbp"$i"msa.zip"
-    wget $URL
-    unzip *.zip
-    rm *.zip
-done
