a2cd23050ba74496693cc9e494902a9d95413978
Hermilo Cortes <hermilocg@hotmail.com>
2025-09-19 11:05:49 -0500

    agrega ggsave a programa

--- src/aranceles/DOM_aranceles.R
+++ src/aranceles/DOM_aranceles.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(hrbrthemes)
 library(viridis)
-
+library(ggrepel)
 
 setwd("/home/milo/Documents/egtp/iniciativas/InvESt_complexity/src/aranceles/output")
 
@@ -27,6 +27,8 @@ data_extensivo = subset(df_hs_2d, year == 2023 & export_rca <1.0)
 data_intensivo_4d = subset(df_hs_4d, year == 2023 & export_rca >=1.0)
 data_extensivo_4d = subset(df_hs_4d, year == 2023 & export_rca <1.0)
 
+ECI_pais = -0.292
+umbral_importaciones = 50
 
 ### INTENSIVO
 ## Export value to USA
@@ -45,7 +47,9 @@ ggplot(data_intensivo,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=50,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)
+
+ggsave("../GTM/hs2_export_value_to_usa_intensivo.png",dpi = 300,  width = 15, height = 9,  bg = 'white')
 
 ## Total export value
 ggplot(data_intensivo, 
@@ -63,7 +67,10 @@ ggplot(data_intensivo,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=50,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)
+
+ggsave("../GTM/hs2_export_value_total_intensivo.png",dpi = 300,  width = 15, height = 9,  bg = 'white')
+
 
 ### EXTENSIVO
 ## Export value to USA
@@ -84,6 +91,8 @@ ggplot(data_extensivo,
   geom_hline(yintercept=50,  color = "red", size = 1.5) + 
   geom_vline(xintercept = -0.19, color = "black", size=1.5)
 
+ggsave("../GTM/hs2_export_value_to_usa_extensivo.png",dpi = 300,  width = 15, height = 9,  bg = 'white')
+
 ## Total export value
 ggplot(data_extensivo, 
        aes(x = pci, y = share_on_total_imports, size = export_value, fill = product_hs92_name_1d, label = product_hs92_name_2d)) +
@@ -100,8 +109,9 @@ ggplot(data_extensivo,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=50,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)
 
+ggsave("../GTM/hs2_export_value_total_extensivo.png",dpi = 300,  width = 15, height = 9,  bg = 'white')
 
 
 ### INTENSIVO HS4
@@ -120,7 +130,7 @@ ggplot(data_intensivo_4d,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=40,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)
 
 ## Total export value
 ggplot(data_intensivo_4d, 
@@ -137,7 +147,7 @@ ggplot(data_intensivo_4d,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=40,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)
 
 ### Etiquetamos puntos en el cuadrante de interés
 labeled_df = subset(data_intensivo_4d, share_on_total_imports>40 & pci > -0.19)
@@ -157,8 +167,11 @@ ggplot(data_intensivo_4d,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=40,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)+ 
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)+ 
   geom_text_repel(data = labeled_df, aes(label = product_hs92_name_4d), size = 3) 
+
+ggsave("../GTM/hs4_intensivo_export_value_to_usa.png",dpi = 300,  width = 15, height = 9,  bg = 'white')
+
 
 ## Total export value
 ggplot(data_intensivo_4d, 
@@ -175,5 +188,7 @@ ggplot(data_intensivo_4d,
   theme_minimal()  +
   guides(fill=guide_legend(override.aes=list(size=6))) + 
   geom_hline(yintercept=40,  color = "red", size = 1.5) + 
-  geom_vline(xintercept = -0.19, color = "black", size=1.5)+ 
+  geom_vline(xintercept = ECI_pais, color = "black", size=1.5)+ 
   geom_text_repel(data = labeled_df, aes(label = product_hs92_name_4d), size = 3) 
+
+ggsave("../GTM/hs4_intensivo_export_value_total.png",dpi = 300,  width = 15, height = 9,  bg = 'white')
